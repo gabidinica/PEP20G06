559ccdeaaf0faa68833294c6e6105c3facc33089
gabidinica <dinica_gabi330@yahoo.com>
2020-11-18 21:35:16 +0200

    new

---
+++ modul1/modul1.py
@@ -0,0 +1,41 @@
+print('Hello World')
+print('Hello World', 'Hello World', 'Hello World', sep='- -')
+print('Hello World', end='$\n')
+
+hello = 'Hello World'
+print(hello,end='\n\n')
+
+print('*Dinica Gabi Andreea', end='*\n')
+
+number1 = 1
+number2 = 2
+number3 = number1 + number2
+print(number3)
+
+number1 = 1
+number2 = 2
+number4= number1 - number2
+print(number4)
+
+number1 = '1'
+number2 = '2'
+number3 = number1 + number2
+print(number3)
+
+number1 = 3
+number2 = '2'
+number3 = number1 * number2
+print(number3)
+
+print()
+number1 = 3
+number2 = '2'
+#number3 = number1/number2
+print(number3)
+
+print(dir(number1))
+print(dir(number2))
+#stdout stderr _> search
+
+
+
