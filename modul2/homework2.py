1f24a5a3667ee16448d3a31a3068b381d41d502e
gabidinica <dinica_gabi30@yahoo.com>
2020-12-01 18:17:22 +0200

    homework2

---
+++ modul2/homework2.py
@@ -0,0 +1,14 @@
+"""Homework 2 - needs to be presented before exam day"""
+
+# 1) Prove "and" operation takes precedence over "or" operation by setting
+# parentheses in (False or False and True or True)
+
+print((True and True) or (False and False))
+
+
+# 2) Get from input two different times in the format hh:mm:ss and print
+# the difference in seconds between them
+firstTimeFormat=input('Insert the first time in the format hh:mm:ss:')
+print('The first time in the format hh:mm:ss is:',firstTimeFormat)
+secondTimeFormat=input('Insert the second time in the format hh:mm:ss:')
+print('Second time in the format hh:mm:ss is:', secondTimeFormat)
